f3e65208dd375f0454118ecee63e18a3e60add6c
Artemides <edmpulasky@gmail.com>
2023-05-25 16:20:02 -0500

    create initial states and join raffle function

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -9,3 +9,35 @@ pragma solidity ^0.8.0;
 // need at least an entrace fee to join
 // keep track of each entering player
 // emit and event when a user enters into the raffle
+
+error Raffle__NoEnoughStartingFee();
+
+contract Raffle {
+    uint256 immutable i_entraceFee;
+    address payable[] s_players;
+
+    event PlayerJoined(address playerAddress);
+
+    modifier enoughStartingFee() {
+        if (msg.value < i_entraceFee) revert Raffle__NoEnoughStartingFee();
+        _;
+    }
+
+    constructor(uint256 entraceFee) {
+        i_entraceFee = entraceFee;
+    }
+
+    function joinRaffle() public payable enoughStartingFee {
+        s_players.push(payable(msg.sender));
+
+        emit PlayerJoined(msg.sender);
+    }
+
+    function getEntraceFee() public view returns (uint256) {
+        return i_entraceFee;
+    }
+
+    function getPlayer(uint256 playerIndex) public view returns (address) {
+        return s_players[playerIndex];
+    }
+}
