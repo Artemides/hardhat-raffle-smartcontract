8b1016c9864f66b18b19c537b0b2ab362a04d894
Artemides <edmpulasky@gmail.com>
2023-05-25 16:17:59 -0500

    define smart contract logic

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -5,3 +5,7 @@ pragma solidity ^0.8.0;
 // the contract needs to pick automatically a  winner, every x random time forever. with no someones' interaction
 
 //Chainlink oracle -> randomness, automated execution(keepers),
+
+// need at least an entrace fee to join
+// keep track of each entering player
+// emit and event when a user enters into the raffle
